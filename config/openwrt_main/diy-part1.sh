5c32030e1b597d8b67e5c10d5d6307a7034cd5d3
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2025-12-06 17:19:55 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-#========================================================================================================================
-# https://github.com/ophub/amlogic-s9xxx-openwrt
-# Description: Automatically Build OpenWrt
-# Function: Diy script (Before Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
-# Source code repository: https://github.com/openwrt/openwrt / Branch: main
-#========================================================================================================================
 
-# Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
-# other
-# rm -rf package/utils/{ucode,fbtest}
-
+# Add luci-app-amlogic
+#git clone https://github.com/ophub/luci-app-amlogic.git   package-temp/luci-app-amlogic
+#mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
+#rm -rf package-temp
+# Add luci-app-openclash
+## 下载OpenClash
+wget https://github.com/vernesong/OpenClash/archive/master.zip
+## 解压
+unzip master.zip
+cp -r OpenClash-master/luci-app-openclash packag/
