93dc9f6c3944d100a3d8551db0fbf257f8677896
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2025-12-20 15:47:26 +0800

    Create diy-part2.sh

---
+++ config/immortalwrt_openwrt-25.12 /diy-part2.sh
@@ -0,0 +1,72 @@
+
+#!/bin/bash
+#========================================================================================================================
+# https://github.com/ophub/amlogic-s9xxx-openwrt
+# Description: Automatically Build OpenWrt
+# Function: Diy script (After Update feeds, Modify the default IP, hostname, theme, add/remove software packages, etc.)
+# Source code repository: https://github.com/immortalwrt/immortalwrt / Branch: master
+#========================================================================================================================
+
+# ------------------------------- Main source started -------------------------------
+#
+# Set default IP address
+default_ip="10.0.0.136"
+ip_regex="^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$"
+# Modify default IP if an argument is provided and it matches the IP format
+[[ -n "${1}" && "${1}" != "${default_ip}" && "${1}" =~ ${ip_regex} ]] && {
+    echo "Modify default IP address to: ${1}"
+    sed -i "/lan) ipad=\${ipaddr:-/s/\${ipaddr:-\"[^\"]*\"}/\${ipaddr:-\"${1}\"}/" package/base-files/*/bin/config_generate
+}
+
+# Add the default password for the 'root' user（Change the empty password to 'password'）
+sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
+# modify rust
+#sed -i 's/--set=llvm.download-ci-llvm=true/--set=llvm.download-ci-llvm=false/g' package/feeds/packages/rust/Makefile
+# Set etc/openwrt_release
+sed -i "s|DISTRIB_REVISION='.*'|DISTRIB_REVISION='R$(date +%Y.%m.%d)'|g" package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEREPO='github.com/immortalwrt/immortalwrt'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCECODE='immortalwrt'" >>package/base-files/files/etc/openwrt_release
+echo "DISTRIB_SOURCEBRANCH='master'" >>package/base-files/files/etc/openwrt_release
+
+# Set ccache
+# Remove existing ccache settings
+sed -i '/CONFIG_DEVEL/d' .config
+sed -i '/CONFIG_CCACHE/d' .config
+# Apply new ccache configuration
+if [[ "${2}" == "true" ]]; then
+    echo "CONFIG_DEVEL=y" >>.config
+    echo "CONFIG_CCACHE=y" >>.config
+    echo 'CONFIG_CCACHE_DIR="$(TOPDIR)/.ccache"' >>.config
+else
+    echo '# CONFIG_DEVEL is not set' >>.config
+    echo "# CONFIG_CCACHE is not set" >>.config
+    echo 'CONFIG_CCACHE_DIR=""' >>.config
+fi
+#
+# ------------------------------- Main source ends -------------------------------
+
+# ------------------------------- Other started -------------------------------
+#
+# Add luci-app-amlogic
+rm -rf package/luci-app-amlogic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
+#
+# Apply patch
+# git apply ../config/patches/{0001*,0002*}.patch --directory=feeds/luci
+#
+# ------------------------------- Other ends -------------------------------
+#删除库中的插件，使用自定义源中的包。
+rm -rf feeds/luci/themes/luci-theme-argon
+rm -rf feeds/luci/applications/luci-app-argon-config
+
+# Add packages
+#添加argon主题
+git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
+# 移除 openwrt feeds 自带的核心库
+rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+
+# 移除 openwrt feeds 过时的luci版本
+rm -rf feeds/luci/applications/luci-app-passwall
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
