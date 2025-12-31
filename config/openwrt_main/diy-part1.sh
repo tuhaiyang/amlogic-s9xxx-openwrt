facaf1f9d907bf903ccd2f87b69bffe16dd5a1f7
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2025-12-31 20:05:53 +0800

    Update diy-part1.sh

--- config/openwrt_main/diy-part1.sh
+++ config/openwrt_main/diy-part1.sh
@@ -4,6 +4,9 @@
 #git clone https://github.com/ophub/luci-app-amlogic.git   package-temp/luci-app-amlogic
 #mv -f package-temp/luci-app-amlogic/luci-app-amlogic package/
 #rm -rf package-temp
+# Add luci-app-passwall
+sed -i '/src-git passwall_packages https://github.com/xiaorouji/openwrt-passwall-packages.git;main/d' feeds.conf.default
+sed -i '/src-git passwall_luci https://github.com/xiaorouji/openwrt-passwall.git;main/d' feeds.conf.default
 # Add luci-app-openclash
 ## 下载OpenClash
 wget https://github.com/vernesong/OpenClash/archive/master.zip
