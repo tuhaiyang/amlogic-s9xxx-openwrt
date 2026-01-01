faf5ee6a88e594c3a63c4966b249b4efa8bcc6e1
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2026-01-01 14:50:46 +0800

    Update diy-part2.sh

--- config/lede_master/diy-part2.sh
+++ config/lede_master/diy-part2.sh
@@ -67,7 +67,7 @@ git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/l
 # 移除 openwrt feeds 自带的核心库
 # rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 # git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
-
+git clone --depth 1 https://github.com/xiaorouji/openwrt-passwall
 # 移除 openwrt feeds 过时的luci版本
 # rm -rf feeds/luci/applications/luci-app-passwall
 # git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
