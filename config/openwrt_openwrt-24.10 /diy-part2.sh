478552d23f3c08035926c1a6cb64cce174bea0c4
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2026-01-09 12:16:52 +0800

    Update diy-part2.sh

--- config/openwrt_openwrt-24.10 /diy-part2.sh
+++ config/openwrt_openwrt-24.10 /diy-part2.sh
@@ -73,7 +73,6 @@ git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwal
 rm -rf feeds/luci/applications/luci-app-passwall
 git clone https://github.com/xiaorouji/openwrt-passwall package/passwall-luci
 rm -rf feeds/packages/net/v2ray-geodata
-git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
 git clone https://github.com/sbwml/v2ray-geodata package/v2ray-geodata
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
