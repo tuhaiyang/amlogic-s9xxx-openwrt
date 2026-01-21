5ce67837cd09dd493a4e246b514499628623adb0
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2026-01-21 14:33:37 +0800

    Update imagebuilder.sh

--- config/imagebuilder/imagebuilder.sh
+++ config/imagebuilder/imagebuilder.sh
@@ -182,7 +182,7 @@ rebuild_firmware() {
     my_packages="\
         acpid attr base-files bash bc blkid block-mount blockd bsdtar btrfs-progs busybox bzip2 \
         cgi-io chattr comgt comgt-ncm containerd coremark coreutils coreutils-base64 coreutils-nohup \
-        coreutils-truncate curl dosfstools dumpe2fs e2freefrag e2fsprogs \
+        coreutils-truncate curl dosfstools dumpe2fs e2freefrag e2fsprogs kmod-nft-socket kmod-nft-tproxy \
         exfat-mkfs f2fs-tools f2fsck fdisk gawk getopt git gzip hostapd-common iconv iw iwinfo jq \
         jshn kmod-brcmfmac kmod-brcmutil kmod-cfg80211 kmod-mac80211 libjson-script liblucihttp \
         liblucihttp-lua losetup lsattr lsblk lscpu mkf2fs mount-utils openssl-util parted \
