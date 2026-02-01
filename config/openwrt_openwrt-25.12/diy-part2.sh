8009adff205ccb1bc86a66a0394253117946daa8
tuhaiyang <39900390+tuhaiyang@users.noreply.github.com>
2026-02-01 15:09:31 +0800

    Update diy-part2.sh

--- config/openwrt_openwrt-25.12/diy-part2.sh
+++ config/openwrt_openwrt-25.12/diy-part2.sh
@@ -64,7 +64,7 @@ rm -rf feeds/luci/themes/luci-theme-openwrt
 git clone --depth 1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone --depth 1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
 #添加passwall2
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
+git clone --depth=1 https://github.com/Openwrt-Passwall/openwrt-passwall2 package/luci-app-passwall2
 # 移除 openwrt feeds 自带的核心库
 rm -rf feeds/packages/net/{xray-core,v2ray-geodata,sing-box,chinadns-ng,dns2socks,hysteria,ipt2socks,microsocks,naiveproxy,shadowsocks-libev,shadowsocks-rust,shadowsocksr-libev,simple-obfs,tcping,trojan-plus,tuic-client,v2ray-plugin,xray-plugin,geoview,shadow-tls}
 git clone https://github.com/Openwrt-Passwall/openwrt-passwall-packages package/passwall-packages
